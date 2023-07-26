337004053084a6e2f9d0e61303bd299eb582b6ae
xu-boyuan <138648548+xu-boyuan@users.noreply.github.com>
2023-07-26 15:23:54 +0900

    s 01

--- js/slide.js
+++ js/slide.js
@@ -4,7 +4,7 @@ $(function(){//おまじない
     slidesToShow: 3,//一度に表示される数
     slidesToScroll:1,//スライドするときの動きの単位
     autoplay: true,//自動スライド
-    autoplaySpeed: 3000,//3000ミリ秒 == 3秒
+    autoplaySpeed: 2000,//3000ミリ秒 == 3秒
     dots:true,// ドット表示
   });
   
